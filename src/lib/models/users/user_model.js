358110d7f9338e62c5c7511866c59e1d8ba3ac13
fenniche <fennicheouahab@MacBook-Air-de-fenniche.local>
2021-07-20 14:32:51 +0200

    update user model using sequilize

---
+++ src/lib/models/users/user_model.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize')
+const sequelize = require('./../../../db/db_connection')
+const User = sequelize.define('user', {
+   user_id:{
+      type:Sequelize.INTEGER,
+      // Increment the value automatically
+      autoIncrement:true,
+      // user_id can not be null.
+      allowNull:false,
+      // To uniquely identify user
+      primaryKey:true
+   },
+
+   firstname: { type: Sequelize.STRING, allowNull:false },
+   lastname: { type: Sequelize.STRING, allowNull:false },
+   email: { type: Sequelize.STRING, allowNull:false },
+   password: { type: Sequelize.STRING, allowNull:false},
+   role: { type: Sequelize.ENUM('Admin', 'User') },
+   role: {
+    type: Sequelize.ENUM,
+    values: ['Admin', 'User']
+  },
+   // Column: Timestamps
+   createdAt: Sequelize.DATE,
+   updatedAt: Sequelize.DATE,
+})
+module.exports = User
