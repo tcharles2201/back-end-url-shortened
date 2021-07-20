ecfa0e98687860f3996d15c3cc3645b99d53a64c
fenniche <fennicheouahab@MacBook-Air-de-fenniche.local>
2021-07-20 14:10:55 +0200

    update db config using sequilize

--- src/db/db_connection.js
+++ src/db/db_connection.js
@@ -1,9 +1,13 @@
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host     : process.env.HOST, 
-  user     : process.env.DB_USER,
-  password : process.env.DB_PASS,
-  database : process.env.DB_DATABASE
-});
-connection.connect();
-module.exports = connection;
+const Sequelize = require('sequelize');
+
+//GET ENV VARIABLES FROM
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.HOST,
+        dialect: 'mysql'
+    });
+
+module.exports = sequelize;
