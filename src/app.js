671e81d5a7f787f17e0802a978caebd3f8319b08
arkpery <thomas.simoes.dev@gmail.com>
2021-07-21 16:20:39 +0200

    UPDATE Port

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
     cors({
-      origin: ["http://localhost:8125"],
+      origin: ["http://localhost:3000"],
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       credentials: true,
        
